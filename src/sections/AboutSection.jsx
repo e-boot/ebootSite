717a4e423a8804be421475c266e79fb6c2058fbc
e-boot <ebootinbox@gmail.com>
2025-05-24 00:42:27 +0100

    remove unnecessary css file, improved tipography in aboutsection

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -5,23 +5,34 @@ export default function AboutSection() {
     <Box
       as="section"
       id='about'
-      scrollSnapAlign="start"
       minH = "100vh"
-       display="flex"
+      display="flex"
       alignItems="center"
       justifyContent="center"
-      py={{base: 16, md:20}}
-      px={6}
+      py={8}
+      px={4}
       bg="white"
       _dark={{ bg: 'gray.800' }}
+      scrollSnapAlign="start"
     >
-      <Stack spacing={8} maxW="2xl" mx="auto" textAlign="center">
-        <Heading size="xl">About Me</Heading>
+      <Stack spacing={6} maxW="2xl" mx="auto" textAlign="center">
+        <Heading 
+          size="2xl"
+          fontWeight="extrabold"
+          letterSpacing="wide"
+          color= "teal.500"
+        >
+          About Me
+        </Heading>
    <Text
-  fontSize="md"
+  fontSize={{base: 'md' , md:'lg'}}
   color="gray.600"
-  _dark={{ color: 'gray.400' }}
->I'm a junior full-stack developer, based in Portugal I enjoy coding in general, but I primarily work with Java and JavaScript. I use Linux daily and love how customizable it is — tweaking and configuring everything. . I'm looking for an opportunity to grow and contribute.
+  _dark={{ color: 'gray.300' }}
+  lineHeight="tall"
+  letterSpacing="wide"
+  fontWeight="medium"
+>
+  I'm a junior full-stack developer, based in Portugal I enjoy coding in general, but I primarily work with Java and JavaScript. I use Linux daily and love how customizable it is — tweaking and configuring everything. . I'm looking for an opportunity to grow and contribute.
 </Text>
       </Stack>
     </Box>
